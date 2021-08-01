f7aa4ff522a082b70357e448419acafea84580e7
Percy Mittagadapala <willi.percy3@gmail.com>
2021-07-31 23:58:31 -0400

    BenfordLab

---
+++ BenfordLab/Benford.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BenfordLab
+{
+    public class BenfordData
+    {
+        public int Digit { get; set; }
+        public int Count { get; set; }
+
+        public BenfordData() { }
+    }
+
+    public class Benford
+    {
+
+        public static BenfordData[] calculateBenford(string csvFilePath)        
+        {
+            // load the data
+            var data = File.ReadAllLines(csvFilePath)
+                .Skip(1) // For header
+                .Select(s => Regex.Match(s, @"^(.*?),(.*?)$"))
+                .Select(data => new
+                {
+                    Country = data.Groups[1].Value,
+                    Population = int.Parse(data.Groups[2].Value)
+                });
+
+            var countriesData = data
+                                .Select(
+                                    countryData => new { 
+                                        Country = countryData.Country, 
+                                        Digit = FirstDigit.getFirstDigit(countryData.Population) 
+                                    })
+                                .ToArray();
+
+            var sortCountryWiseCount = countriesData
+                                       .GroupBy(country => country.Digit)
+                                       .Select(country => (Digit: country.Key, Count: country.Count()))
+                                       .OrderByDescending(country => country.Digit).ToArray();
+
+            List<BenfordData> m = new List<BenfordData>();
+            foreach (var i in sortCountryWiseCount)
+            {
+                m.Add(
+                    new BenfordData
+                    {
+                        Digit = i.Digit,
+                        Count = i.Count
+                    }
+                );
+            }
+
+            return m.ToArray();
+        }
+    }
+}
---
+++ BenfordLab/FirstDigit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenfordLab
+{
+    public class FirstDigit
+    {
+        /* Given an integer X, this method returns the first digit
+         * 
+         * For example,
+         *   5 ==> returns 5
+         *   10 ==> returns 1
+         *   654321 ==> returns 6
+         */
+        public static int getFirstDigit(int digit) {
+            int number = digit;
+            while(number >= 10)
+            {
+                number = number / 10;
+            }
+            return number;
+        }    
+    }
+}
